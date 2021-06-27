47b7970883d60bc54302fbe288d3025ba5bc1949
tonysousa <tony.chaves14@gmail.com>
2021-06-27 07:19:31 -0300

    Clean Home page and  pass useAuth

--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -8,11 +8,6 @@ import '../../styles/auth.scss';
 import { Button } from '../../components/Button/Button';
 import { useAuth } from '../../Hooks/useAuth';
 
-
-
-
-
-
 export function Home(){
     const history = useHistory();
     const {user, signInWithGoogle} = useAuth();
